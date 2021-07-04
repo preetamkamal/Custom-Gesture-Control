df9313ac1646fe35725d2fec18667d703aa946bf
Kamal Preetam <preetamkamal@gmail.com>
2021-07-04 11:54:15 +0530

    Added cloud login

--- gui/linkers/gesture-linker.js
+++ gui/linkers/gesture-linker.js
@@ -1,19 +1,19 @@
 let { PythonShell } = require("python-shell");
-var path = require("path");
-
+let path = require("path");
+let gestureSetId = "def";
 function get_gesture_input() {
-  var gesture0 = document.getElementById("element0").value;
-  var gesture1 = document.getElementById("element1").value;
-  var gesture2 = document.getElementById("element2").value;
-  var gesture3 = document.getElementById("element3").value;
-  var gesture4 = document.getElementById("element4").value;
-  var gesture5 = document.getElementById("element5").value;
-  var gesture6 = document.getElementById("element6").value;
-  var gesture7 = document.getElementById("element7").value;
-  var gesture8 = document.getElementById("element8").value;
-  var gesture9 = document.getElementById("element9").value;
-  var gesture10 = document.getElementById("element10").value;
-  var gesture11 = document.getElementById("element11").value;
+  let gesture0 = document.getElementById("element0").value;
+  let gesture1 = document.getElementById("element1").value;
+  let gesture2 = document.getElementById("element2").value;
+  let gesture3 = document.getElementById("element3").value;
+  let gesture4 = document.getElementById("element4").value;
+  let gesture5 = document.getElementById("element5").value;
+  let gesture6 = document.getElementById("element6").value;
+  let gesture7 = document.getElementById("element7").value;
+  let gesture8 = document.getElementById("element8").value;
+  let gesture9 = document.getElementById("element9").value;
+  let gesture10 = document.getElementById("element10").value;
+  let gesture11 = document.getElementById("element11").value;
   
 
   document.getElementById("element0").disabled = true;
@@ -29,7 +29,8 @@ function get_gesture_input() {
   document.getElementById("element10").disabled = true;
   document.getElementById("element11").disabled = true;
   document.getElementById("submit-button").disabled = true;
-  var options = {
+ 
+  let options = {
     scriptPath: path.join(__dirname, "/../engine/"),
     args: [
       gesture0,
@@ -56,7 +57,7 @@ function get_gesture_input() {
     console.log("results: %j", results);
   });
   pyshell.on("message", function (message) {
-    swal(message);
+    // swal(message);
     document.getElementById("element0").disabled = false;
     document.getElementById("element1").disabled = false;
     document.getElementById("element2").disabled = false;
@@ -71,11 +72,208 @@ function get_gesture_input() {
     document.getElementById("element11").disabled = false;
     document.getElementById("submit-button").disabled = false;
   });
-
+  console.log('Hi')
   pyshell.end(function (err, code, signal) {
     if (err) throw err;
     // console.log("The exit code was: " + code);
     // console.log("The exit signal was: " + signal);
-    // console.log("finished");
+    console.log("finished");
   });
+}
+
+ // --------------FIREBASE CONFIG----------------------
+  const firebaseConfig = {
+    apiKey: "AIzaSyA3Bzvz8ogR-rqkKc4mQib2HoyY9t5jtO8",
+    authDomain: "gesture-navigation-system.firebaseapp.com",
+    projectId: "gesture-navigation-system",
+    storageBucket: "gesture-navigation-system.appspot.com",
+    messagingSenderId: "366819068277",
+    appId: "1:366819068277:web:75d3d4ec831dc94f59eb3c",
+    measurementId: "G-WB4572JMRZ",
+  };
+
+  firebase.initializeApp(firebaseConfig);
+
+  let cloudDB = firebase.firestore();
+  
+// --------------------WRITE TO FIREBASE --------------
+function Add_Doc_WithAutoID() {
+  let gesture0 = document.getElementById("element0").value;
+  let gesture1 = document.getElementById("element1").value;
+  let gesture2 = document.getElementById("element2").value;
+  let gesture3 = document.getElementById("element3").value;
+  let gesture4 = document.getElementById("element4").value;
+  let gesture5 = document.getElementById("element5").value;
+  let gesture6 = document.getElementById("element6").value;
+  let gesture7 = document.getElementById("element7").value;
+  let gesture8 = document.getElementById("element8").value;
+  let gesture9 = document.getElementById("element9").value;
+  let gesture10 = document.getElementById("element10").value;
+  let gesture11 = document.getElementById("element11").value;
+
+
+  // --- ADD RANDOM ID
+  cloudDB.collection("gestures").add(
+    {
+      gesture0: gesture0,
+      gesture1: gesture1,
+      gesture2: gesture2,
+      gesture3: gesture3,
+      gesture4: gesture4,
+      gesture5: gesture5,
+      gesture6: gesture6,
+      gesture7: gesture7,
+      gesture8: gesture8,
+      gesture9: gesture9,
+      gesture10: gesture10,
+      gesture11: gesture11,
+      id: gestureSetId
+    }
+  ).then(function (docRef) {
+    console.log('Doc written with ID', docRef);
+  }).catch(function (err) {
+    console.log("error found at 131", err)
+  })
+}
+
+function Add_Doc_WithID() {
+  let gesture0 = document.getElementById("element0").value;
+  let gesture1 = document.getElementById("element1").value;
+  let gesture2 = document.getElementById("element2").value;
+  let gesture3 = document.getElementById("element3").value;
+  let gesture4 = document.getElementById("element4").value;
+  let gesture5 = document.getElementById("element5").value;
+  let gesture6 = document.getElementById("element6").value;
+  let gesture7 = document.getElementById("element7").value;
+  let gesture8 = document.getElementById("element8").value;
+  let gesture9 = document.getElementById("element9").value;
+  let gesture10 = document.getElementById("element10").value;
+  let gesture11 = document.getElementById("element11").value;
+
+  // --- ADD CUSOM ID
+  cloudDB
+    .collection("gestures").doc(gestureSetId)
+    .set({
+      gesture0: gesture0,
+      gesture1: gesture1,
+      gesture2: gesture2,
+      gesture3: gesture3,
+      gesture4: gesture4,
+      gesture5: gesture5,
+      gesture6: gesture6,
+      gesture7: gesture7,
+      gesture8: gesture8,
+      gesture9: gesture9,
+      gesture10: gesture10,
+      gesture11: gesture11,
+      id: gestureSetId,
+    })
+    .then(function (docRef) {
+      console.log("Doc written with ID", docRef);
+    })
+    .catch(function (err) {
+      console.log("error found at 131", err);
+    });
+}
+
+
+//  --------------UPDATE DATA _----------------------
+
+function Update_Gesture() {
+  let gesture0 = document.getElementById("element0").value;
+  let gesture1 = document.getElementById("element1").value;
+  let gesture2 = document.getElementById("element2").value;
+  let gesture3 = document.getElementById("element3").value;
+  let gesture4 = document.getElementById("element4").value;
+  let gesture5 = document.getElementById("element5").value;
+  let gesture6 = document.getElementById("element6").value;
+  let gesture7 = document.getElementById("element7").value;
+  let gesture8 = document.getElementById("element8").value;
+  let gesture9 = document.getElementById("element9").value;
+  let gesture10 = document.getElementById("element10").value;
+  let gesture11 = document.getElementById("element11").value;
+
+  // --- ADD CUSOM ID
+  cloudDB
+    .collection("gestures")
+    .doc(gestureSetId)
+    .update({
+      gesture0: gesture0,
+      gesture1: gesture1,
+      gesture2: gesture2,
+      gesture3: gesture3,
+      gesture4: gesture4,
+      gesture5: gesture5,
+      gesture6: gesture6,
+      gesture7: gesture7,
+      gesture8: gesture8,
+      gesture9: gesture9,
+      gesture10: gesture10,
+      gesture11: gesture11,
+ 
+    })
+    .then(function (docRef) {
+      console.log("Doc written with ID", docRef);
+    })
+    .catch(function (err) {
+      console.log("error found at 131", err);
+    });
+}
+
+
+function RetriveData() {
+  let gesture0Entity = document.getElementById("element0");
+  let gesture1Entity = document.getElementById("element1");
+  let gesture2Entity = document.getElementById("element2");
+  let gesture3Entity = document.getElementById("element3");
+  let gesture4Entity = document.getElementById("element4");
+  let gesture5Entity = document.getElementById("element5");
+  let gesture6Entity = document.getElementById("element6");
+  let gesture7Entity = document.getElementById("element7");
+  let gesture8Entity = document.getElementById("element8");
+  let gesture9Entity = document.getElementById("element9");
+  let gesture10Entity = document.getElementById("element10");
+  let gesture11Entity = document.getElementById("element11");
+
+
+
+
+  cloudDB
+    .collection("gestures")
+    .doc(gestureSetId)
+    .get().then((docRef) => {
+      if (docRef.exists) {
+        gesture0Entity.value = docRef.data().gesture0;
+        gesture1Entity.value = docRef.data().gesture1;
+        gesture2Entity.value = docRef.data().gesture2;
+        gesture3Entity.value = docRef.data().gesture3;
+        gesture4Entity.value = docRef.data().gesture4;
+        gesture5Entity.value = docRef.data().gesture5;
+        gesture6Entity.value = docRef.data().gesture6;
+        gesture7Entity.value = docRef.data().gesture7;
+        gesture8Entity.value = docRef.data().gesture8;
+        gesture9Entity.value = docRef.data().gesture9;
+        gesture10Entity.value = docRef.data().gesture10;
+        gesture11Entity.value = docRef.data().gesture11;
+        console.log("DATA READ SUCCESSFUL")
+      } else {
+        console.log("does not exist")
+      }
+    }).catch(function (err) {
+      console.log("error found at 263", err);
+    })
+}
+
+
+  // -------------------BUTTON EVENT-------------------
+function add_to_firebase(){
+  // Add_Doc_WithAutoID();
+  Add_Doc_WithID();
+}
+
+function read_from_firebase() {
+  RetriveData();
+}
+function updateText(val) {
+  gestureSetId = val;
 }
